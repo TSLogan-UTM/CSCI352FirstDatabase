cac9d9560a972be0fe9d64e79ac1cbb51ab7b1a4
Thomas <thosloga@CSG1-522517>
2019-09-19 18:22:17 -0500

    Add project files.

---
+++ CSCI352FirstDatabase/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSCI352FirstDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OleDbConnection cn;
+            cn = new OleDbConnection(<ENTER STRING HERE>);
+
+        }
+
+        private void AssetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
